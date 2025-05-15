d245576377d71bd76b74a988327b069ec265fa5a
marco76tv <marco.sottana@gmail.com>
2025-05-15 12:28:04 +0200

    .

--- app/Filament/Resources/RatingMorphResource/Pages/ListRatingMorphs.php
+++ app/Filament/Resources/RatingMorphResource/Pages/ListRatingMorphs.php
@@ -20,11 +20,15 @@ class ListRatingMorphs extends XotBaseListRecords
         ];
     }
 
+<<<<<<< Updated upstream
 <<<<<<< HEAD
     public function getTableColumns(): array
 =======
     public function getListTableColumns(): array
 >>>>>>> 380edba (.)
+=======
+    public function getTableColumns(): array
+>>>>>>> Stashed changes
     {
         return [
             'id' => Columns\TextColumn::make('id')
