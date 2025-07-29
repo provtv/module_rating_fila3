2eaad501c4eb680f4e6bfa8c5afa159335e4b6b6
marco76tv <marco.sottana@gmail.com>
2025-07-29 15:08:12 +0200

    .

--- app/Models/RatingMorph.php
+++ app/Models/RatingMorph.php
@@ -85,8 +85,6 @@ class RatingMorph extends BaseMorphPivot
         'value',
         'is_winner',
         'reward',
-        'value_buy',
-        'value_sell',
     ];
     // -------- RELATIONSHIP -----------
 
