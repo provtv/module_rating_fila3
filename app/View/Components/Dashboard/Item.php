5168601fabaefde8e03ff39aae9dfcf91f9b0dc5
Malebestia <malebestia@yahoo.it>
2025-08-21 12:33:57 +0200

    .

--- app/View/Components/Dashboard/Item.php
+++ app/View/Components/Dashboard/Item.php
@@ -13,7 +13,11 @@ use Illuminate\View\Component;
  */
 class Item extends Component
 {
+<<<<<<< HEAD
     public function render(): string
+=======
+    public function render()
+>>>>>>> 95ef831 (.)
     {
         return '';
     }
